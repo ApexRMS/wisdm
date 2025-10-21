f0c8bb6e5ed77ab2393295e4a757a9f216078bd4
spearmangillman <schuyler.pearman-gillman@apexrms.com>
2025-10-21 11:05:11 -0300

    Create 00-constants.R

---
+++ src/00-constants.R
@@ -0,0 +1,7 @@
+## -------------------------
+## wisdm - constants
+## ApexRMS, October 2025
+## -------------------------
+
+# gam smoothing methods
+gamSmoothingMethodCW <- data.frame(codeTerm = c("ts", "cc", "re","gp"), displayTerm = c("Thin plate regression splines with shrinkage-to-zero",  "Cyclic cubic regression splines", "Random effects", "Gaussian process smooths"))
